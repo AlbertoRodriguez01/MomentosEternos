d220b71b43cfbaa5169fc8df30c5afa1bfa3420c
AlbertoRodriguez01 <37781005+AlbertoRodriguez01@users.noreply.github.com>
2024-11-05 20:32:40 -0600

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,7 +1,7 @@
 const HeroSection = () => {
     return (
         <div>
-           <img className="w-screen h-50 lg:h-96 object-cover" src="src/assets/img/1.png" alt="" />
+           <img className="w-screen h-50 lg:h-96 object-cover" src="https://firebasestorage.googleapis.com/v0/b/momentos-eternos-5e14f.appspot.com/o/1.png?alt=media&token=4208f68e-f812-46db-a1c2-68b87b1d53f6" alt="" />
         </div>
     );
 }
